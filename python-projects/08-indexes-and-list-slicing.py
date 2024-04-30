74aad25549619db2ca7e5cfc301dcedaba9c9407
jack <jrfisch95@gmail.com>
2024-04-30 11:17:56 -0400

    add lists notes

---
+++ python-projects/08-indexes-and-list-slicing.py
@@ -0,0 +1,36 @@
+# Exercise 1
+list_of_lists = [[0,2],[4,6],[8,10],[12,14]]
+
+# Exercise 2
+print(list_of_lists[0])
+
+# Exercise 3
+print(list_of_lists[-1][-1])
+
+# Exercise 4
+str_list = ['chair', 'table', 'desk', 'lamp', 'bed']
+
+# Exercise 5
+print(str_list[-5])
+
+# Exercise 6
+print((
+  "Most people own at least "
+  + str(list_of_lists[0][1])
+  + " "
+  + str_list[0]
+  + "s."
+))
+
+# Exercise 7
+float_list = [0.98,8.76,6.54,4.32]
+
+# Exercise 8
+print(float_list[1:])
+
+# Exercise 9
+print(float_list[1:3])
+
+# Exercise 10
+print(float_list[:2])
+
